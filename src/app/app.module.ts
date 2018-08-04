1bdd2bc62c84faa11f04587839d7a851e6f4841e
Muktadir Imam Jan <muktadirimam@gmail.com>
2018-08-04 13:10:37 +0600

    app module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,56 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { MatButtonModule, MatSelectModule } from "@angular/material";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatButtonModule } from "@angular/material";
-import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireAuthModule } from "angularfire2/auth";
+import { environment } from "./../environments/environment";
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
 
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./main/header/header.component";
+import { FooterComponent } from "./main/footer/footer.component";
+
+import { HomeComponent } from "./main/home/home.component";
+import { ProductsComponent } from "./main/products/products.component";
+import { ShoppingCartComponent } from "./main/shopping-cart/shopping-cart.component";
+import { CheckOutComponent } from "./main/check-out/check-out.component";
+import { OrderSuccessComponent } from "./main/order-success/order-success.component";
+import { MyOrdersComponent } from "./main/my-orders/my-orders.component";
+
+import { AdminProductsComponent } from "./admin/admin-products/admin-products.component";
+import { AdminOrdersComponent } from "./admin/admin-orders/admin-orders.component";
+import { LoginComponent } from "./login/login.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatSelectModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

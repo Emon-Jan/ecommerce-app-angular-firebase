e68286d018a49f6a0dfb98270dc2047910af6b69
Muktadir Imam Jan <muktadirimam@gmail.com>
2018-08-06 03:10:14 +0600

    style js deleted

--- src/assets/style.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var elems = document.querySelectorAll(".sidenav");
-  var instances = M.Sidenav.init(elems);
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-  var drop = document.querySelectorAll(".dropdown-trigger");
-  var inst = M.Dropdown.init(drop);
-});

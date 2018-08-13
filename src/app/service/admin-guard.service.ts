8d0bb3d2c3cc834c626cf599baf5f8d4d1741d29
Muktadir Imam Jan <muktadirimam@gmail.com>
2018-08-13 15:17:40 +0600

    admin-guard and user service added

---
+++ src/app/service/admin-guard.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from "@angular/router";
+
+import { AuthService } from "./auth.service";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class AdminGuard implements CanActivate {
+  constructor(private authService: AuthService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | boolean {
+    return this.authService.eUser.map(user => user.isAdmin);
+  }
+}
---
+++ src/app/service/user.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+
+import { Observable } from "rxjs";
+import * as firebase from "firebase";
+import { User } from "../model/user.model";
+import { AngularFireDatabase } from "angularfire2/database";
+
+@Injectable()
+export class UserService {
+  constructor(private fireDb: AngularFireDatabase) {}
+
+  saveUser(user: firebase.User) {
+    this.fireDb
+      .object("/user/" + user.uid)
+      .update({
+        userName: user.displayName,
+        userEmail: user.email
+      })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }
+
+  getUser(uid: String): Observable<User> {
+    let obj: Observable<any>;
+    obj = this.fireDb.object("/user/" + uid).valueChanges();
+    return obj;
+  }
+}

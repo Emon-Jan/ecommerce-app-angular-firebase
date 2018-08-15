6ab750c94444ca469fb726aa469dcba988bcff08
Muktadir Imam Jan <muktadirimam@gmail.com>
2018-08-15 12:34:05 +0600

    admin-product updated

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { Product } from "./../../model/product.model";
+import { ProductService } from "./../../service/product.service";
 
 @Component({
-  selector: 'app-admin-products',
-  templateUrl: './admin-products.component.html',
-  styleUrls: ['./admin-products.component.css']
+  selector: "app-admin-products",
+  templateUrl: "./admin-products.component.html",
+  styleUrls: ["./admin-products.component.css"]
 })
 export class AdminProductsComponent implements OnInit {
-
-  constructor() { }
+  products: any[];
+  filteredProducts: Product[];
+  constructor(private productService: ProductService) {}
 
   ngOnInit() {
+    this.productService.getProductsFromFirebase().subscribe(res => {
+      this.filteredProducts = this.products = res;
+    });
   }
 
+  filter(query: string) {
+    this.filteredProducts = query
+      ? this.products.filter(items =>
+          items.payload
+            .val()
+            .title.toLowerCase()
+            .includes(query.toLowerCase())
+        )
+      : this.products;
+  }
 }

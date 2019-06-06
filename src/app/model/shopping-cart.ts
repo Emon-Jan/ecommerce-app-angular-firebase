cbf0be308a5efe816bde334c61dc4ebfba4fda9e
Muktadir Imam Jan <muktadirimam@gmail.com>
2019-06-06 08:14:37 +0600

    cart async pipe used

--- src/app/model/shopping-cart.ts
+++ src/app/model/shopping-cart.ts
@@ -1,30 +1,37 @@
 import { ShoppingCartItem } from "./shoppingcart-item";
+import { Product } from "./product.model";
 
 export class ShoppingCart {
   items: ShoppingCartItem[] = [];
 
-  constructor(public itemsMap: {[productId: string]: ShoppingCartItem}) {
-    for (const productId in itemsMap){
-      let item = itemsMap[productId];
+  constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
+    this.itemsMap = this.itemsMap || {};
+    for (const productId in itemsMap) {
+      let item = this.itemsMap[productId];
+      let x = new ShoppingCartItem();
+      Object.assign(x, item);
+      x.key = productId;
       if (item.quantity !== 0) {
-        this.items.push(new ShoppingCartItem(item.product, item.quantity));
+        this.items.push(x);
       }
     }
   }
 
-  get totalItemsPrice(){
+  getQuantity(product: Product) {
+    const item = this.itemsMap[product.key];
+    return item ? item.quantity : 0;
+  }
+
+  get totalItemsPrice() {
     let sum = 0;
-    for (const productId in this.items)
-      sum += this.items[productId].totalPrice;
+    for (const productId in this.items) sum += this.items[productId].totalPrice;
     return sum;
   }
 
-  get totalItemsCount() {
+  get totalItemsCount(): number {
     let itemCount = 0;
     for (const prodId in this.itemsMap)
       itemCount += this.itemsMap[prodId].quantity;
     return itemCount;
   }
 }
-
-
